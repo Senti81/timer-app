ed36e2537f83ac9d884b110988d64ffb3fc3b030
Matthias Büttgenbach <matthias.buettgenbach@cologne-intelligence.de>
2021-08-11 11:34:51 +0200

    added counter + stylings

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -2,6 +2,17 @@
 	<v-container fluid>
     <v-layout>
       <v-flex xs12 pa-3>
+        <div class="label">
+          <div class="score">
+            <h1>{{scoreHome}}</h1>
+          </div>
+          <div class="score">
+            <h1>:</h1>
+          </div>
+          <div class="score">
+            <h1>{{scoreAway}}</h1>
+          </div>
+        </div>
         <v-card>
           <v-card-title class="secondary white--text font-weight-light display-2 text-uppercase justify-center">
             {{ timer }}
@@ -32,6 +43,31 @@
               >
                 Stop
               </v-btn>
+              <div class="flex-container">
+                <div class="left">
+                  <v-btn 
+                    fab
+                    class="success"
+                    @click="increaseHome(9)"
+                    >+</v-btn>
+                  <v-btn
+                    fab
+                    class="error"
+                    @click="decreaseHome(0)"
+                    >-</v-btn>
+                </div>
+                <div class="right">
+                  <v-btn
+                    fab
+                    class="success"
+                    @click="increaseAway(9)"
+                    >+</v-btn>
+                  <v-btn
+                    fab
+                    class="error"
+                    @click="decreaseAway(0)">-</v-btn>
+                </div>
+              </div>     
             </v-list>
           </v-card-text>
         </v-card>
@@ -42,7 +78,62 @@
 
 <script>
 export default {
+  data () {
+    return {
+      scoreHome: 0,
+      scoreAway: 0,
+    }
+  },
+  methods: {
+    increaseHome(limit) {
+      if(this.scoreHome < limit) this.scoreHome++;
+    },
+    decreaseHome(limit) {
+      if (this.scoreHome > limit) this.scoreHome--;
+    },
+    increaseAway(limit) {
+      if(this.scoreAway < limit) this.scoreAway++;
+    },
+    decreaseAway(limit) {
+      if (this.scoreAway > limit) this.scoreAway--;
+    }
+  },
   name: 'Timer',
   props: ['timer', 'state']
 }
 </script>
+
+<style scoped>
+  .label {
+    text-align: center;    
+  }
+
+  .score {
+    display: inline-block;
+    width: 15%;
+    height: 150px;
+    margin: 20px;
+  }
+
+  h1 {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-size: 72px;
+  }
+
+  .flex-container {
+    display: flex;
+  }
+
+  .left{
+    display: flex;
+    flex-direction: row;
+    width: 50%;
+  }
+
+  .right{
+    display: flex;
+    flex-direction: row-reverse;
+    width: 50%;
+  }
+
+</style>
